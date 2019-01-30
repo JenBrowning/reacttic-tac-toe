711c21fd3853ba4dc2d66efac2d3c27f05136530
Jennifer Browning <browningjen@yahoo.com>
2019-01-30 12:27:07 -0500

    Updates

---
+++ precache-manifest.860f26fe8c58ba3349ee8626f9c10366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d54184da5e6dcbb40f8a",
+    "url": "/reacttic-tac-toe/static/js/runtime~main.d54184da.js"
+  },
+  {
+    "revision": "bc254ca414a8aff8011a",
+    "url": "/reacttic-tac-toe/static/js/main.bc254ca4.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/reacttic-tac-toe/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "bc254ca414a8aff8011a",
+    "url": "/reacttic-tac-toe/static/css/main.44593a23.chunk.css"
+  },
+  {
+    "revision": "857ec80348b4fce048a7010f40444b63",
+    "url": "/reacttic-tac-toe/index.html"
+  }
+];
